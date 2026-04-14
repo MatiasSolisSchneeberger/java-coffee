ffc63b3dd42a2049e34cb7fe36230c8bca8cfe92
Matias Solis Schneeberger <matias.solis.sch@gmail.com>
2026-04-14 15:36:53 -0300

    creando rutas de paginas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/quienes-somos', function () {
+    return view('quienes-somos');
+});
+
+Route::get('/comercialización', function () {
+    return view('comercializacion');
+});
+
+Route::get('/información-de-contactos', function () {
+    return view('informacion-de-contactos');
+});
+
+Route::get('/terminos-y-usos', function () {
+    return view('terminos-y-usos');
+});
+
+Route::get('/catalogo-de-productos', function () {
+    return view('catalogo-de-productos');
+});
+
+Route::get('/consultas', function () {
+    return view('consultas');
+});
+
+Route::get('/registro-de-clientes', function () {
+    return view('registro-de-clientes');
+});
+
+Route::get('/formulario-de-login', function () {
+    return view('formulario-de-login');
+});
