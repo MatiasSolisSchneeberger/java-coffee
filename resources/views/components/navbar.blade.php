436771aa317f1abe010212972ba378ab3e48f22c
Matias Solis Schneeberger <matias.solis.sch@gmail.com>
2026-04-14 15:36:25 -0300

    creando Navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <nav>
+        <ul>
+            <li><a href="/quienes-somos">¿Quiénes somos?</a></li>
+            <li><a href="/comercializacion">Comercialización</a></li>
+            <li><a href="/informacion-de-contactos">Información de contactos</a></li>
+            <li><a href="/terminos-y-usos">Términos y usos</a></li>
+            <li><a href="/catalogo-de-productos">Catálogo de productos</a></li>
+            <li><a href="/consultas">Consultas</a></li>
+            <li><a href="/registro-de-clientes">Registro de clientes</a></li>
+            <li><a href="/formulario-de-login">Formulario de login</a></li>
+        </ul>
+    </nav>
+</div>
