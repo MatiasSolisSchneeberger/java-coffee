83aa238e951e5307d53ee4a24a4815082c8c7b28
Matias Solis Schneeberger <matias.solis.sch@gmail.com>
2026-04-15 00:32:14 -0300

    creando loco

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,10 @@
+<span class="logo">
+    {{-- TODO: 
+        Elegir cual queda mejor
+    --}}
+    <img src={{ asset('svg/tabler-icon-coffee.svg') }} />
+    <img src={{ asset('svg/tabler-icon-mug.svg') }} />
+    <span>
+        Java Coffee
+    </span>
+</span>
