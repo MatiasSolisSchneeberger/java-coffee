5098d9a8ff023577b0b531d08036a3cae43d755c
Matias Solis Schneeberger <matias.solis.sch@gmail.com>
2026-04-16 11:37:44 -0300

    creando componente card

---
+++ resources/views/components/ui/card.blade.php
@@ -0,0 +1,11 @@
+<article class="card">
+    <img src="/images/hero-section.png" alt="">
+    <h3>Producto 1</h3>
+    <span>
+        <p>Este es un producto con ciertas características muy interesante</p>
+    </span>
+    <x-ui.button>
+        Ver Producto
+        <x-icons.chevron-right />
+    </x-ui.button>
+</article>
