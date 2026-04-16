d513bcb8911c62af365f1550f3cc4b2c717ac5a5
Matias Solis Schneeberger <matias.solis.sch@gmail.com>
2026-04-16 11:38:48 -0300

    creando lista de productos

---
+++ resources/views/components/sections/lista-productos.blade.php
@@ -0,0 +1,10 @@
+<section class="lista-productos-container">
+
+    <h2>Nuestros Productos</h2>
+    <section class="lista-productos">
+        <x-ui.card title="Producto 1" description="Este es un producto con ciertas características muy interesante"
+            image="/images/hero-section.png" />
+        <x-ui.card />
+        <x-ui.card />
+    </section>
+</section>
