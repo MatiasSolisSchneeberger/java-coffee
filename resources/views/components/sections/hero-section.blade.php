5ada3c479a6f5e2da2e0dc0aede32aaebbe775fa
Matias Solis Schneeberger <matias.solis.sch@gmail.com>
2026-04-16 11:37:58 -0300

    creando hero section

---
+++ resources/views/components/sections/hero-section.blade.php
@@ -0,0 +1,9 @@
+<section class="hero-section">
+    <h1>Java Coffee</h1>
+    <p>El mejor café de la ciudad</p>
+
+    <x-ui.button>
+        Ver productos
+        <x-icons.chevron-right />
+    </x-ui.button>
+</section>
