ccf9ea917f9e1718fddcb821b6ca7df28ca253da
Matias Solis Schneeberger <matias.solis.sch@gmail.com>
2026-04-21 10:23:53 -0300

    creando footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,43 @@
+<footer class="footer">
+    <div class="footer-container">
+        <div class="footer-brand">
+            <x-logo />
+            <p class="footer-description">El combustible indispensable para tus líneas de código.</p>
+        </div>
+
+        <div class="footer-links">
+            <div class="footer-column">
+                <h4>Navegación</h4>
+                <ul>
+                    <li><a href="/">Inicio</a></li>
+                    <li><a href="/quienes-somos">Quiénes Somos</a></li>
+                    <li><a href="/información-de-contactos">Contacto</a></li>
+                </ul>
+            </div>
+            <div class="footer-column">
+                <h4>Tienda</h4>
+                <ul>
+                    <li><a href="/catalogo-de-productos">Catálogo de Productos</a></li>
+                    <li><a href="/comercialización">Comercialización</a></li>
+                </ul>
+            </div>
+            <div class="footer-column">
+                <h4>Soporte</h4>
+                <ul>
+                    <li><a href="/consultas">Consultas Frecuentes</a></li>
+                    <li><a href="/terminos-y-usos">Términos y Usos</a></li>
+                </ul>
+            </div>
+            <div class="footer-column">
+                <h4>Cuenta</h4>
+                <ul>
+                    <li><a href="/formulario-de-login">Iniciar Sesión</a></li>
+                    <li><a href="/registro-de-clientes">Registrarse</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>&copy; {{ date('Y') }} Java Coffee. Todos los derechos reservados.</p>
+    </div>
+</footer>
